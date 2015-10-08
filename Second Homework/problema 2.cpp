e1a05989e794e4f25b890eedb706a0a3cbb8b6f7
JordanMoshe <jordan.mendoza@ucsp.edu.pe>
2015-10-07 19:26:32 -0500

    problema 2

--- Second Homework/problema 2.cpp
+++ Second Homework/problema 2.cpp
@@ -2,22 +2,35 @@
 
 using namespace std;
 
-string no_repetitions(char* letra,string frase);
+string no_repetitions(char *letra);
 
 int main(){
     string frase;
-    cin>>frase;
+    getline(cin,frase);
+    cout<<frase;
     char *letra;
     letra=&frase[0];
-    no_repetitions(letra,frase);
+    no_repetitions(&letra);
     cout<<frase<<endl;
     return 0;
 }
 
-string no_repetitions(char* letra,string frase){
-    //char vect[20];
-    short int tam=0;
-    while (*letra){
+string no_repetitions(char *letra){
+    char vect[20];
+    short int i;
+    while(*letra){
+        vect[i]=*letra;
+        i++;
+    }
+    for(int j=0;j<20;j++){
+        for(int k=j;j<20;k++){
+
+        }
+    }
+
+
+/*    short int tam=0;
+    while (*(letra+1)!=0){
         letra++;
         tam++;
     }
@@ -25,9 +38,10 @@ string no_repetitions(char* letra,string frase){
 
     letra=&frase[0];
     cout<<*letra<<endl;
-    for (short int j=0;j<tam;j++){
+    for (short int j=0;j<tam-1;j++){
         if (*letra==*(letra+j)){
             *(letra+j)=0;
         }
     }
-}
+
+}*/
